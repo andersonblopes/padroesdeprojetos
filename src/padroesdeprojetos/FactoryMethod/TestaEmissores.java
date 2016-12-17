547c439728ba2aa73a3f75b6678fbdbe2b0c38aa
anderson <anderson@anderson-VirtualBox>
2016-12-17 11:40:13 -0300

    Refatoração e correção de encoding

--- src/padroesdeprojetos/FactoryMethod/TestaEmissores.java
+++ src/padroesdeprojetos/FactoryMethod/TestaEmissores.java
@@ -2,32 +2,32 @@ package padroesdeprojetos.FactoryMethod;
 
 public class TestaEmissores {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Emissor emissor1 = new EmissorSMS();
-		emissor1.enviamensagem("K19 treinamentos!");
+	Emissor emissor1 = new EmissorSMS();
+	emissor1.enviamensagem("K19 treinamentos!");
 
-		Emissor emissor2 = new EmissorEmail();
-		emissor2.enviamensagem("K19 treinamentos!");
+	Emissor emissor2 = new EmissorEmail();
+	emissor2.enviamensagem("K19 treinamentos!");
 
-		Emissor emissor3 = new EmissorJMS();
-		emissor3.enviamensagem("K19 treinamentos!");
+	Emissor emissor3 = new EmissorJMS();
+	emissor3.enviamensagem("K19 treinamentos!");
 
-		/* UTILIZANDO A CLASSE DE CRIA«√O DOS EMISSORES */
-		System.out.println("\n\nUTILIZANDO A CLASSE DE CRIA«√O DOS EMISSORES\n\n ");
-		EmissorCreator creator = new EmissorCreator();
+	/* UTILIZANDO A CLASSE DE CRIA√á√ÉO DOS EMISSORES */
+	System.out.println("\nUTILIZANDO A CLASSE DE CRIA√á√ÉO DE EMISSORES\n");
+	EmissorCreator creator = new EmissorCreator();
 
-		// SMS
-		Emissor emissor4 = creator.create(EmissorCreator.SMS);
-		emissor4.enviamensagem("K19 Treinamentos");
+	// SMS
+	Emissor emissor4 = creator.create(EmissorCreator.SMS);
+	emissor4.enviamensagem("K19 Treinamentos");
 
-		// EMAIL
-		Emissor emissor5 = creator.create(EmissorCreator.EMAIL);
-		emissor5.enviamensagem("K19 Treinamentos");
+	// EMAIL
+	Emissor emissor5 = creator.create(EmissorCreator.EMAIL);
+	emissor5.enviamensagem("K19 Treinamentos");
 
-		// JMS
-		Emissor emissor6 = creator.create(EmissorCreator.JMS);
-		emissor6.enviamensagem("K19 Treinamentos");
-	}
+	// JMS
+	Emissor emissor6 = creator.create(EmissorCreator.JMS);
+	emissor6.enviamensagem("K19 Treinamentos");
+    }
 
 }
