209c2d56e1e1721b41a0fb152fc5a312d02fbb05
anderson <anderson@anderson-VirtualBox>
2017-01-15 11:54:41 -0300

    Implementação de exercicio de fixação.

--- src/padroesdeprojetos/abstractfactory/TestaVisaComunicadorFactory.java
+++ src/padroesdeprojetos/abstractfactory/TestaVisaComunicadorFactory.java
@@ -4,17 +4,30 @@ public class TestaVisaComunicadorFactory {
 
     public void abstractFactory() {
 	System.out.println("\n=== PADRÃO ABSTRACT FACTORY ===");
+	testaFabricaVisa();
+	testaFabricaMastercard();
+    }
 
+    public void testaFabricaVisa() {
+	System.out.println("FÁBRICA VISA");
 	ComunicadorFactory comunicadorFactory = new VisaComunicadorFactory();
-
 	String transacao = " Valor =560; Senha =123 ";
 	Emissor emissor = comunicadorFactory.createEmissor();
 	emissor.enviaMensagem(transacao);
-
 	Receptor receptor = comunicadorFactory.createReceptor();
 	String mensagem = receptor.recebeMensagem();
 	System.out.println(mensagem);
+    }
 
+    public void testaFabricaMastercard() {
+	System.out.println("FÁBRICA MASTERCARD");
+	ComunicadorFactory comunicadorFactory = new MastercardComunicadorFactory();
+	String transacao = " Valor = 1000; Senha = 456 ";
+	Emissor emissor = comunicadorFactory.createEmissor();
+	emissor.enviaMensagem(transacao);
+	Receptor receptor = comunicadorFactory.createReceptor();
+	String mensagem = receptor.recebeMensagem();
+	System.out.println(mensagem);
     }
 
 }
